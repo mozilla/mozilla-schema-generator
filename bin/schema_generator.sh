abac1a742847a614e504c945efbf4cdfdac1b1fb
Frank Bertsch <fbertsch@mozilla.com>
2019-06-19 10:07:48 -0700

    Add minimal main ping schema
    - Reorder allowlist
    - Add schemas dir which overwrites schemas
    - Add main ping to allowlist

--- bin/schema_generator.sh
+++ bin/schema_generator.sh
@@ -130,6 +130,9 @@ function main() {
     # Generate new schemas
     mozilla-schema-generator generate-glean-pings --out-dir .
 
+    # Overwrite schemas in schemas dir
+    cp -TRv $BASE_DIR/mozilla-schema-generator/schemas/ ./
+
     # Remove all non-json schemas (e.g. parquet)
     find . -not -name "*.schema.json" -type f -exec rm {} +
 
