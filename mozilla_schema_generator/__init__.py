b3be349839584df0d6630d47ddb339bfa73b474e
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-06-07 05:16:15 -0700

    Add autogenerated documentation via pdoc (#200)
    * Add pdoc for api docs
    
    * Fix bad run step
    
    * Consolidate build steps into single step
    
    * Add separate docs build stage
    
    * fix typo in module name
    
    * Use toplevel docs workspace folder
    
    * Use tmp folder for files
    
    * Use /tmp folder for persistent work
    
    * Use correct location for dest
    
    * pin gh-pages to 3.0.0
    
    * Add ssh fingerprint
    
    * Use directory instead of installed module
    
    * Deploy on commits to main

--- mozilla_schema_generator/__init__.py
+++ mozilla_schema_generator/__init__.py
@@ -3,6 +3,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Generate schemas for mozilla-pipeline-schemas.
+
+.. include:: ../README.md
+"""
+__docformat__ = "restructuredtext"
 
 __author__ = "Frank Bertsch"
 __email__ = "frank@mozilla.com"
