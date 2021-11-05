d63f33b7d17cdfa97fe6337985c7a9e4a44f9262
Jeff Klukas <jeff@klukas.net>
2021-11-05 13:00:28 -0400

    Add more comprehensive commentary in the code

--- mozilla_schema_generator/glean_ping.py
+++ mozilla_schema_generator/glean_ping.py
@@ -188,6 +188,13 @@ class GleanPing(GenericPing):
                 loc: m.clone(new_table_group=ping) for loc, m in config.matchers.items()
             }
 
+            # Four newly introduced metric types were incorrectly deployed
+            # as repeated key/value structs in all Glean ping tables existing prior
+            # to November 2021. We maintain the incorrect fields for existing tables
+            # by disabling the associated matchers, but all new Glean pings will have
+            # the matchers enabled, meaning these jwe, labeled_rate, text, and url types
+            # will only show up in the schema if the ping is defined to contain metrics
+            # of those types.
             # See https://bugzilla.mozilla.org/show_bug.cgi?id=1737656
             bq_identifier = "{bq_dataset_family}.{bq_table}".format(**pipeline_meta)
             if bq_identifier in self.bug_1737656_affected_tables:
