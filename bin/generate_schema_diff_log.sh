b347c6fe0018dc2a996fb722474de87532c2a2f3
= <=>
2021-11-24 15:41:38 +0100

    -\_(00)_/-

---
+++ bin/generate_schema_diff_log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source mozilla-schema-generator/bin/schema_generator.sh
+# -e causes the container to exit immediately
+set +e
+setup_git
+setup_mps
+mozilla-schema-generator/bin/test_validate main  >> tmp.log
+sed -n '/---/,$p' < tmp.log > schema_diff.log
